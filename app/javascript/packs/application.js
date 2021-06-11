5c17bc8738fd9aa03a4c2eae5d7a6ba98328df43
macnick <nick@macexperts.gr>
2021-06-11 13:58:51 +0300

    Add gem devise-semantified, styled flash messages and page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,7 @@ ActiveStorage.start()
 
 $(document).on('turbolinks:load', () => {
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function () {
+    $(this).closest('.message').transition('fade')
+  })
 });
