30c5eb2f00f3d00b3b0d2847502691f0661df37f
macnick <nick@macexperts.gr>
2021-06-25 09:49:48 +0300

    Style the search stock form

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,8 +2,9 @@ class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.iex[:token],
-      # secret_token: 'sk_ed98ef3591784076ad2eb5be46504d00',
+      publishable_token: Rails.application.credentials.iex_client[:token],
+      # publishable_token: 'pk_640fb043666045cbb284550d311ebf95' ,
+      secret_token: 'sk_ed98ef3591784076ad2eb5be46504d00',
       endpoint: 'https://cloud.iexapis.com/v1'
     )
     client.quote(ticker_symbol)
