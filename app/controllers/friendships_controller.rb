3dd308fb642bc84e37bb18f4c7538f61bfe22fa2
macnick <nick@macexperts.gr>
2021-07-01 19:49:56 +0300

    Add the capability to follow users

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,13 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "You follow the user"
+    else
+      flash[:alert] = "There was an error. Please try again"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
